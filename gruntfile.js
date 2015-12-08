629b1aeef7503b576bd1c265fcc48459c6d49289
danielpaquette <dm.paquette@gmail.com>
2015-12-07 22:20:43 -0500

    Updated file structure, added bower, proper linting, various other changes

--- gruntfile.js
+++ gruntfile.js
@@ -1,54 +1,87 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks("grunt-sass");
+	grunt.loadNpmTasks("grunt-contrib-clean");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-scss-lint");
 
-  grunt.initConfig({
-    sass: {
-      options: {
-        sourceMap: true,
-        outputStyle: 'compressed'
-      },
-      website: {
-        files: {
-          'resources/css/danpaquette.min.css' : 'resources/src/scss/init.scss'
-        }
-      }
-    },
-    clean: {
-      css: [
-        'resources/css'
-      ],
-      js: [
-        'resources/js'
-      ]
-    },
-    uglify: {
-      options: {
-        sourceMap: true,
-        mangle: {
-          except: ['jQuery']
-        },
-        compress: true
-      },
-      js: {
-        files: {
-          'resources/js/danpaquette.min.js': [
-            'resources/src/lib/jquery/jquery-2.1.4.min.js',
-            'resources/src/js/**/*.js'
-          ]
-        }
-      }
-    },
-    jshint: {
-      all: ['resources/src/js/**/*.js']
-    }
-  });
+	grunt.initConfig({
+		/**
+		 * Lints scss.
+		 */
+		scsslint: {
+			allFiles: [
+				"src/scss/**/*.scss"
+			],
+			options: {
+				config: "config/build/scsslint.yml",
+				colorizeOutput: true,
+				maxBuffer: 3000 * 1024
+			}
+		},
 
-  grunt.registerTask('build-css', ['clean:css','sass:website']);
-  grunt.registerTask('build-js', ['jshint','clean:js', 'uglify:js']);
+		/**
+		 * Lints javascript.
+		 */
+		jshint: {
+			options: {
+				"jshintrc": "config/build/.jshintrc"
+			},
+			all: ["src/js/**/*.js"]
+		},
 
-  grunt.registerTask('travis', ['build-css', 'build-js']);
+		/**
+		 * Cleans folders to be rebuilt.
+		 */
+		clean: {
+			css: [
+				"resources/css"
+			],
+			js: [
+				"resources/js"
+			]
+		},
+
+		/**
+		 * Builds scss.
+		 */
+		sass: {
+			options: {
+				sourceMap: true,
+				outputStyle: "compressed"
+			},
+			website: {
+				files: {
+					"resources/css/main.min.css" : "src/scss/init.scss"
+				}
+			}
+		},
+
+		/**
+		 * Concatenates and uglifies javascript.
+		 */
+		uglify: {
+			options: {
+				sourceMap: true,
+				mangle: {
+					except: ["jQuery"]
+				},
+				compress: true
+			},
+			js: {
+				files: {
+					"resources/js/main.min.js": [
+						"bower_components/jquery/dist/jquery.min.js",
+						"src/js/**/*.js"
+					]
+				}
+			}
+		}
+	});
+
+	grunt.registerTask("build-css", ["scsslint", "clean:css", "sass:website"]);
+	grunt.registerTask("build-js", ["jshint", "clean:js", "uglify:js"]);
+
+	grunt.registerTask("travis", ["build-css", "build-js"]);
 };
