7dbca0329f05d474991f4527b8cb195b50852122
Dan Paquette <dm.paquette@gmail.com>
2016-01-03 16:56:14 -0500

    Initial file structure.

--- gruntfile.js
+++ gruntfile.js
@@ -1,87 +1,222 @@
 module.exports = function(grunt) {
 
-	grunt.loadNpmTasks("grunt-sass");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-jshint");
-	grunt.loadNpmTasks("grunt-scss-lint");
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("grunt-contrib-clean");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-scss-lint");
+  grunt.loadNpmTasks("grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-cache-breaker");
 
-	grunt.initConfig({
-		/**
-		 * Lints scss.
-		 */
-		scsslint: {
-			allFiles: [
-				"src/scss/**/*.scss"
-			],
-			options: {
-				config: "config/build/scsslint.yml",
-				colorizeOutput: true,
-				maxBuffer: 3000 * 1024
-			}
-		},
+  grunt.initConfig({
 
-		/**
-		 * Lints javascript.
-		 */
-		jshint: {
-			options: {
-				"jshintrc": "config/build/.jshintrc"
-			},
-			all: ["src/js/**/*.js"]
-		},
+    /**
+     * Copies assets from dependencies into public.
+     */
+    copy: {
+      public: {
+        files: [{
+          expand: true,
+          cwd: "resources",
+          src: [
+            "img/**/*"
+          ],
+          dest: "dist/resources"
+        },{
+          expand: true,
+          cwd: "resources/html",
+          src: [
+            "**/*"
+          ],
+          dest: "dist/"
+        },{
+          expand: true,
+          cwd: "config/public",
+          src: [
+            "**/*"
+          ],
+          dest: "dist/"
+        }]
+      }
+    },
 
-		/**
-		 * Cleans folders to be rebuilt.
-		 */
-		clean: {
-			css: [
-				"resources/css"
-			],
-			js: [
-				"resources/js"
-			]
-		},
+    /**
+     * Lints scss.
+     */
+    scsslint: {
+      allFiles: [
+        "resources/scss/**/*.scss"
+      ],
+      options: {
+        config: "config/build/scsslint.yml",
+        colorizeOutput: true,
+        maxBuffer: 3000 * 1024
+      }
+    },
 
-		/**
-		 * Builds scss.
-		 */
-		sass: {
-			options: {
-				sourceMap: true,
-				outputStyle: "compressed"
-			},
-			website: {
-				files: {
-					"resources/css/main.min.css" : "src/scss/init.scss"
-				}
-			}
-		},
+    /**
+     * Lints javascript.
+     */
+    jshint: {
+      options: {
+        "jshintrc": "config/build/.jshintrc"
+      },
+      all: ["resources/js/**/*.js"]
+    },
 
-		/**
-		 * Concatenates and uglifies javascript.
-		 */
-		uglify: {
-			options: {
-				sourceMap: true,
-				mangle: {
-					except: ["jQuery"]
-				},
-				compress: true
-			},
-			js: {
-				files: {
-					"resources/js/main.min.js": [
-						"bower_components/jquery/dist/jquery.min.js",
-						"src/js/**/*.js"
-					]
-				}
-			}
-		}
-	});
+    /**
+     * Deletes all files and directories as specified.
+     */
+    clean: {
+      dist: [
+        "dist"
+      ],
+      css: [
+        "dist/resources/css"
+      ],
+      js: [
+        "dist/resources/js"
+      ]
+    },
 
-	grunt.registerTask("build-css", ["scsslint", "clean:css", "sass:website"]);
-	grunt.registerTask("build-js", ["jshint", "clean:js", "uglify:js"]);
+    /**
+     * Compiles scss files.
+     */
+    sass: {
+      options: {
+        sourceMap: false,
+        outputStyle: "compressed"
+      },
+      resources: {
+        files: {
+          "dist/resources/css/main.min.css" : "resources/scss/init.scss"
+        }
+      }
+    },
 
-	grunt.registerTask("travis", ["build-css", "build-js"]);
+    /**
+     * Post processor for automatically adding browser prefixes.
+     */
+    postcss: {
+      options: {
+        map: false,
+        processors: [
+          require("autoprefixer")({
+            browsers: ["last 2 versions"]
+          })
+        ]
+      },
+      dist: {
+        src: "dist/resources/css/main.min.css"
+      }
+    },
+
+    /**
+     * Concatenates and minifies CSS files.
+     */
+    cssmin: {
+      options: {
+        sourceMap: false
+      },
+      vendor: {
+        files: {
+          "dist/resources/css/vendor.min.css": [
+            "bower_components/normalize-css/normalize.css"
+          ]
+        }
+      }
+    },
+
+
+    /**
+     * Concatenates and uglifies javascript.
+     */
+    uglify: {
+      options: {
+        separator: ";\n",
+        sourceMap: false,
+        compress: true
+      },
+      resources: {
+        files: {
+          "dist/resources/js/main.min.js": [
+            "resources/js/**/*.js"
+          ]
+        }
+      },
+      vendor: {
+        files: {
+          "dist/resources/js/vendor.min.js": [
+
+          ]
+        }
+      }
+    },
+
+    /**
+     * Searches /public/index.html and appends a timestamp to style/script includes forcing the
+     * browser to reload those resources
+     */
+    cachebreaker: {
+      css: {
+        options: {
+          match: [
+            "vendor.min.css",
+            "main.min.css"
+          ],
+          position: "append"
+        },
+        files: {
+          src: ["dist/index.html"]
+        }
+      },
+      js: {
+        options: {
+          match: [
+            "vendor.min.js",
+            "main.min.js"
+          ],
+          position: "append"
+        },
+        files: {
+          src: ["dist/index.html"]
+        }
+      }
+    }
+  });
+
+  /**
+   * CSS
+   */
+  grunt.registerTask("build-css", [
+    "scsslint",
+    "clean:css",
+    "sass:resources",
+    "postcss:dist",
+    "cssmin:vendor",
+  ]);
+
+  /**
+   * JS
+   */
+  grunt.registerTask("build-js", [
+    "jshint",
+    "clean:js",
+    "uglify:resources",
+    "uglify:vendor",
+  ]);
+
+  /**
+   * JS
+   */
+  grunt.registerTask("default", [
+    "clean:dist",
+    "build-css",
+    "build-js",
+    "copy:public",
+    "cachebreaker:css",
+    "cachebreaker:js"
+  ]);
 };
